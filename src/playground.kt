4ce0dadd2e50203abe4a84898d4e4345a23b5199
Vincent Nigro <vnigro5743@gmail.com>
2021-12-17 19:30:12 -0500

    Booleans

--- src/playground.kt
+++ src/playground.kt
@@ -20,7 +20,11 @@ fun main(args: Array<String>)
     dataTypeFunc()
 
     operatorsFunc()
-    
+
+    booleanFunc()
+
+    stringsFunc()
+
     args.forEach{ print(it + " ") }
 }
 
@@ -180,6 +184,48 @@ fun operatorsFunc()
     println("x.or(y) = " + x.or(y)) // 61 = 0011 1101
     println("x.xor(y) = " + x.xor(y)) // 49 = 0011 0001
     println("x.inv() = " + x.inv()) // -61 1100 0011
+    println()
+    println("-".repeat(MAX_LINE_OUT))
+    println()
+}
+
+/**
+ * A boolean variable can be created using the Boolean keyword and this variable can only take the values either true
+ * or false (Kotlin's type inference can also figure it out).
+ */
+fun booleanFunc()
+{
+    // Kotlin boolean operators: &&, ||, !
+    var x: Boolean = true
+    var y: Boolean = false
+
+    println("Kotlin Booleans")
+    println("Kotlin Boolean Operator Expressions")
+
+    println("x && y = " + (x && y))
+    println("x || y = " + (x || y))
+    println("!y = " + (!y))
+    println()
+
+    println("Kotlin Logical Operations")
+    println("Logical functions cannot perform short-circuit evaluation but always evaluate both operands.")
+    println("x.and(y) = " + x.and(y))
+    println("x.or(y) = " + x.or(y))
+    println("x.xor(y) = " + x.xor(y))
+    println("x.not() = " + x.not())
+    println()
+
+    println()
+    println("-".repeat(MAX_LINE_OUT))
+    println()
+}
+
+/**
+ *
+ */
+fun stringsFunc()
+{
+
     println()
     println("-".repeat(MAX_LINE_OUT))
     println()
