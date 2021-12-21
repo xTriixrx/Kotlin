8f6bcf29cf1ec73f8fd617f889caa8e81f802a63
Vincent Nigro <vnigro5743@gmail.com>
2021-12-20 20:46:21 -0500

    Arrays

--- src/playground.kt
+++ src/playground.kt
@@ -1,5 +1,7 @@
 package com.qfi
 
+import java.util.Arrays
+
 // https://www.tutorialspoint.com/kotlin/index.htm
 
 /* This is a multi-line comment and it can span
@@ -24,6 +26,8 @@ fun main(args: Array<String>)
     booleanFunc()
 
     stringsFunc()
+
+    arrayFunc()
 
     args.forEach{ print(it + " ") }
 }
@@ -280,6 +284,63 @@ fun stringsFunc()
     println("getOrNull(100) - ${name.getOrNull(100)}")
 
     // Kotlin toString() returns a string representation of an object
+
+    println()
+    println("-".repeat(MAX_LINE_OUT))
+    println()
+}
+
+/**
+ *  Arrays are used to store multiple items of the same data-type in a single variable, such as an integer or string
+ *  under a single variable name. For example, if we need to store names of 1000 employees, then instead of creating
+ *  1000 different string variables, we can simply define an array of strings. Like any other modern programming
+ *  language, Kotlin also supports arrays and provide a wide range of array properties and support functions.
+ */
+fun arrayFunc()
+{
+    // To create an array in Kotlin, we use the arrayOf() function, and place the values in a comma-separated list
+    val fruits = arrayOf("Apple", "Mango", "Banana", "Orange")
+
+    // Optionally, we can provide a data type as follows:
+    val stringFruits = arrayOf<String>("Apple", "Mango", "Banana", "Orange")
+    print("Array of Fruits - ")
+    stringFruits.forEach{ print("$it ") }
+    println()
+
+    // Alternatively, the arrayOfNulls() function can be used to create an array of a given size filled with null elements
+    val nullFruits = arrayOfNulls<String>(4)
+    print("Array of Nulls - ")
+    nullFruits.forEach { print("$it ") }
+    println()
+
+    // Kotlin also has some built-in factory methods to create arrays of primitive data types
+    val numArray = intArrayOf(1, 2, 3, 4)
+    print("Integer Array - ")
+    numArray.forEach { print("$it ") }
+    println()
+
+    val byteArray = byteArrayOf(0x1, 0x2, 0x3)
+    print("Byte Array - ")
+    byteArray.forEach { print("$it ") }
+    println()
+
+    val charArray = charArrayOf('A', 'B', 'C')
+    print("Char Array - ")
+    charArray.forEach { print("$it ") }
+    println()
+
+    val shortArray = shortArrayOf(1, 2, 3)
+    print("Short Array - ")
+    shortArray.forEach { print("$it ") }
+    println()
+
+    val longArray = longArrayOf(1, 2, 3)
+    print("Long Array - ")
+    longArray.forEach { print("$it ") }
+    println()
+
+    // We can access an array element by using the index number inside square brackets.
+    // Kotlin array index starts with 0
 
     println()
     println("-".repeat(MAX_LINE_OUT))
