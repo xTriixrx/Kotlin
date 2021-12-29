ab70f4e4c7fb11a41c8bf16dc2ba2262102ade0c
Vincent Nigro <vnigro5743@gmail.com>
2021-12-28 21:04:04 -0500

    kotlin lists

--- src/playground.kt
+++ src/playground.kt
@@ -46,6 +46,8 @@ fun main(args: Array<String>)
 
     breakAndContinueControlFlowFunc()
 
+    collectionsFunc()
+
     if (args.isNotEmpty())
     {
         args.forEach{ print("$it ") }
@@ -1003,5 +1005,208 @@ fun breakAndContinueControlFlowFunc()
 
     println()
     println("-".repeat(MAX_LINE_OUT))
+    println()
+}
+
+/**
+ * Collections are a common concept for most programming languages. A collection usually contains a number of objects
+ * of the same type and Objects in a collection are called elements or items. The Kotlin Standard Library provides a
+ * comprehensive set of tools for managing collections. The following collection types are relevant for Kotlin:
+ *
+ * Kotlin List - List is an ordered collection with access to elements by indices. Elements can occur more than once
+ * in a list.
+ * Katlin Set - Set is a collection of unique elements which means a group of objects without repetitions.
+ * Kotlin Map - Map (or dictionary) is a set of key-value pairs. Keys are unique, and each of them maps to exactly one
+ * value.
+ *
+ * Kotlin provides both collection/immutable and mutable collections.
+ *
+ * Kotlin Immutable Collection
+ * Immutable collection or simply calling a Collection interface provides read-only methods which means once a collection
+ * is created, we cannot change it because there is no method available to change the object created.
+ * Collection Types                     Methods of Immutable Collection
+ *      List                                    listOf() | listOf<T>()
+ *      Map                                     mapOf()
+ *      Set                                     setOf()
+ *
+ * Kotlin Mutable Collection
+ * Mutable collections provide both read and write methods
+ * Collection Types                     Methods of Muttable Collection
+ *      List                                    ArrayList<T>() | arrayListOf() | mutableListOf()
+ *      Map                                     HashMap | hashMapOf() | mutableMapOf()
+ *      Set                                     hashSetOf() | mutableSetOf()
+ */
+fun collectionsFunc()
+{
+    println("collectionsFunc started.")
+    println()
+
+    listsFunc()
+
+    setsFunc()
+
+    mapsFunc()
+
+    println()
+    println("collectionsFunc finished.")
+
+    println()
+    println("-".repeat(MAX_LINE_OUT))
+    println()
+}
+
+/**
+ * Kotlin list is an ordered collection of items. A Kotlin list can be either mutable (mutableListOf) or read-only (listOf).
+ * The elements of a list can be accessed using indices. Kotlin mutable or immutable lists can have duplicate elements.
+ */
+fun listsFunc()
+{
+    println("listsFunc started.")
+    println()
+
+    // For list creation, use the standard library functions listOf() for read-only lists and mutableListOf() for
+    // mutable lists. To prevent unwanted modifications, obtain read-only views of mutable lists by casting them to List.
+
+    val theList = listOf("one", "two", "three", "four")
+    println("theList - $theList")
+    var theMutableList = mutableListOf("one", "two", "three", "four")
+    println("theMutableList - $theMutableList")
+
+    // There are various ways to loop through a Kotlin list:
+    // Using Iterator
+    val itr = theList.iterator()
+    while (itr.hasNext())
+    {
+        print("${itr.next()} ")
+    }
+    println()
+
+    // Using for loop
+    for (value in theList)
+    {
+        print("$value ")
+    }
+    println()
+
+    // Using forEach (Note: here it works like this operator in Java)
+    theList.forEach { print("$it ") }
+    println()
+
+    // We can use the size property to get the total number of elements in a list
+    println("Size of the list: ${theList.size}")
+
+    // The 'in' operator can be used to check the existence of an element in a list
+    if ("two" in theList)
+    {
+        println("Two is in the list.")
+    }
+
+    // The contain() method can also be used to check the existence of an element in a list
+    if (theList.contains("two"))
+    {
+        println("Two is in the list.")
+    }
+
+    // The isEmpty() method returns true if the collection is empty (contains no elements), false otherwise.
+    if (!theList.isEmpty())
+    {
+        println("This list is not empty.")
+    }
+
+    // The indexOf() method returns the index of the first occurrence of the specified element in the list, or -1
+    // if the specified element is not contained in the list
+    println("Index of 'two': ${theList.indexOf("two")}")
+
+    // The get() method ca nbe used to get the element at the specified index in the list. First element index will be 0
+    println("Element at 3rd position: ${theList.get(2)}")
+
+    // We can use + operator to add two or more lists into a single list. This will add the second list into the first
+    // list, even duplicated elements will be added.
+    val firstList = listOf("one", "two", "three")
+    val secondList = listOf("four", "five", "six")
+    val resultList = firstList + secondList
+    println("Result of list addition: $resultList")
+
+    // We can use the - operator to subtract a list from another list. This operation will remove the common elements
+    // from the first list and will return the result.
+    val thirdList = listOf("one", "five", "six")
+    val subResultList = firstList - thirdList
+    println("Result of list subtraction: $subResultList")
+
+    // We can obtain a sublist from a given list using the slice() method which makes use of ranges
+    val sliceResultList = resultList.slice(2..4)
+    println("Result of list slice: $sliceResultList")
+
+    // We can use filterNotNull() method to remove null elements from a Kotlin list
+    val nullList = listOf("one", "two", null, "four", "five")
+    val nullRemoveResultList = nullList.filterNotNull()
+    println("Result of null element removal: $nullRemoveResultList")
+
+    // We can use filter() to filter out the elements matching with the given predicate
+    val numList = listOf(10, 20, 30, 31, 40, 50, -1, 0)
+    val filterResult = numList.filter { it > 30 }
+    println("Result of filter where element must be greater than 30: $filterResult")
+
+    // We can use the drop() method to drop the first N elements from a list
+    val dropResult = numList.drop(3)
+    println("Result of dropping first 3 elements: $dropResult")
+
+    // We can use groupBy() method to group the elements matching with the given predicate
+    val groupResult = numList.groupBy { it % 3 }
+    println("Result of groupBy 'it % 3': $groupResult")
+
+    // We can use map() method to map all elements using the providied function
+    val mapResult = numList.map { it / 3 }
+    println("Result of map 'it / 3': $mapResult")
+
+    // We can use chunked() method to create chunks of the given size from a list.
+    val chunkedResult = numList.chunked(3)
+    println("Result of chunked(3): $chunkedResult")
+
+    // We can use windowed() method to a list of element ranges by moving a sliding window of a given size of a
+    // collection of elements
+    val windowedResult = numList.windowed(3)
+    println("Result of windowed(3): $windowedResult")
+
+    // By default, the sliding window moves one step further each time but we can change that by passing a custom step value:
+    val windowedStepResult = numList.windowed(3, 3)
+    println("Result of windowed(3, 3): $windowedStepResult")
+
+    // We can create a mutable list using mutableListOf(), later we can use add() to add more elements in the same list,
+    // and we can use remove() to remove the elements from the list
+    val mutableList = mutableListOf(10, 20, 30)
+    mutableList.add(40)
+    mutableList.add(50)
+    println("Mutable List after add: $mutableList")
+    mutableList.remove(10)
+    mutableList.remove(30)
+    println("Mutable List after remove: $mutableList")
+
+    println()
+    println("listsFunc finished.")
+    println()
+}
+
+fun setsFunc()
+{
+    println("setsFunc started.")
+    println()
+
+
+
+    println()
+    println("setsFunc finished.")
+    println()
+}
+
+fun mapsFunc()
+{
+    println("mapsFunc started.")
+    println()
+
+
+
+    println()
+    println("mapsFunc finished.")
     println()
 }
